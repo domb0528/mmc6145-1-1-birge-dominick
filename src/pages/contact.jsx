0cb3555055ec8c6c3a7075f87a7041453b4d7ea0
Dominick birge <dominickbirge@Dominicks-MacBook-Pro.local>
2024-01-12 12:25:32 -0500

    added some CSS need to continue adding more CSS

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,9 +1,10 @@
-import Header from "../components/header"
 export default function Contact () {
     return  (
         
         <main>
          <h1>Contact page</h1>
+
+         <div className="contact">
 
          <p>To reach me click the links below</p>
 
@@ -11,7 +12,9 @@ export default function Contact () {
 
          <p>Here is my Repositories <a href="https://github.com/domb0528" target="blank">Git-Hub</a></p>
          
-   
+         </div>
+         
          </main>
+         
     ) 
 }
