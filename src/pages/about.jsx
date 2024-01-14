e77d9e03b55f8e2b9cce8ad1123c00a273cf277c
Dominick birge <dominickbirge@Dominicks-MacBook-Pro.local>
2024-01-14 15:59:41 -0500

    added css to the pagde and background, and img files.

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -2,7 +2,9 @@ export default function About () {
     return ( 
         <body>
             <h1>About page</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur labore, eos perferendis quis odio, dolore impedit amet autem, fugit obcaecati itaque? Recusandae pariatur quis maxime sequi officiis! Id, architecto nisi.</p>
+            <p className="about me">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur labore, eos perferendis quis odio, dolore impedit amet autem, fugit obcaecati itaque? Recusandae pariatur quis maxime sequi officiis! Id, architecto nisi. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem quidem sit voluptas amet illo alias, animi fugiat non voluptatibus blanditiis quaerat, ut, maxime numquam. Labore perferendis recusandae tempore omnis consequatur. Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel rerum deleniti voluptates, ratione itaque temporibus facere quod consequatur veritatis eius earum minus mollitia ullam porro nostrum? Maxime quam fugiat nemo. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Praesentium reprehenderit sed veritatis saepe dolores fugit doloribus quaerat laboriosam tenetur harum quasi debitis neque, voluptas commodi dignissimos corporis et! Blanditiis, odit?</p>
+
+        <img src="images/photo-of-me.jpg" alt="Dominick Birge" width="220" height="280" />
         </body>
     
     )
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -6,13 +6,13 @@ export default function Home() {
   return (
    
     <main>
-      <h1 className={styles.placeholder}>I'm the home page!</h1>
+      <h1 className={styles.placeholder}>Dominck Birge's Portfolio</h1>
       <h2>Here are some of my projects I have worked on</h2>
-      <p>click the links to go to my vercel account</p>
+      <h2>Click the links to go to my vercel account</h2>
 
-      <Project title="Project 1" information="My Capstone Project"/>
+      <Project className={styles.placeholder} title="Project 1" information="My Capstone Project"/>
       <Project title="Project 2" information="My placeholder"/>
-      <Project title="Project 2" information="My placeholder"/>
+      <Project title="Project 3" information="My placeholder"/>
     </main>
   )
 }
