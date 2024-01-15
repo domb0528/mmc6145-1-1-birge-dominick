3978999133f485bbfd9fe6cb7dd1938ddc55125e
Dominick birge <dominickbirge@Dominicks-MacBook-Pro.local>
2024-01-14 19:16:41 -0500

    fixed the image on aboput page test 2

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -4,7 +4,7 @@ export default function About () {
             <h1>About page</h1>
             <p className="about me">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur labore, eos perferendis quis odio, dolore impedit amet autem, fugit obcaecati itaque? Recusandae pariatur quis maxime sequi officiis! Id, architecto nisi. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem quidem sit voluptas amet illo alias, animi fugiat non voluptatibus blanditiis quaerat, ut, maxime numquam. Labore perferendis recusandae tempore omnis consequatur. Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel rerum deleniti voluptates, ratione itaque temporibus facere quod consequatur veritatis eius earum minus mollitia ullam porro nostrum? Maxime quam fugiat nemo. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Praesentium reprehenderit sed veritatis saepe dolores fugit doloribus quaerat laboriosam tenetur harum quasi debitis neque, voluptas commodi dignissimos corporis et! Blanditiis, odit? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque omnis voluptatem ad. Ut, nam. Minus porro rem explicabo. Assumenda animi illum aspernatur, sequi cumque esse a molestiae maiores natus praesentium?</p>
 
-        <img src="/photo-of-me.jpg" alt="Dominick Birge" width="220" height="280" />
+        <img src="/aboutme.jpg" alt="Dominick Birge" width="220" height="280" />
         </body>
     
     )
